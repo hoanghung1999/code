ca2b3b82b5937ded0f2452b9f9a6f7ac06116c3e
hoanghung1999 <hungcn8b@gmail.com>
2020-02-11 16:05:41 +0700

    code 1

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+public class test {
+    final static Logger logger= Logger.getLogger(test.class);
+    public static void main(String[] args) {
+        System.out.println("chao dai k :)");
+        logger.warn("loi canh bao");
+
+    }
+}
